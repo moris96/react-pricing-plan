18a763842ff714b633d6257ec69422e226fad5fa
moris96 <moriskhoudari@gmail.com>
2023-12-12 14:10:47 -0500

    added neccesary components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import PricingCard from "./components/PricingCard/PricingCard";
+import PricingPlan from "./components/PricingPlan/PricingPlan";
+
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Works</h1>
+      <h1>App.js</h1>
+      <PricingCard/>
+      <PricingPlan/>
     </div>
   );
 }
---
+++ src/components/PricingPlan/PricingPlan.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PricingPlan = () => {
+  return (
+    <div>
+      <h1>Pricing Plan</h1>
+    </div>
+  )
+}
+
+export default PricingPlan
