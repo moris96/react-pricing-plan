490e8954ab80b0f048ca71ccb2a7764dc8b7a4cd
moris96 <moriskhoudari@gmail.com>
2023-12-14 09:49:45 -0500

    updated readme

--- src/components/PricingCard/PricingCard.js
+++ src/components/PricingCard/PricingCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './PricingCard.css';
 
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 
 const PricingCard = ({title, price, storage, users}) => {
@@ -20,7 +22,10 @@ const PricingCard = ({title, price, storage, users}) => {
             <div className='card-users-allowed'>{users} users in total</div>
         </div>
         {/* text popup when pressing button */}
-        <button className='card-btn'>READ MORE</button>
+        {/* <button className='card-btn'>READ MORE</button> */}
+        <Popup trigger={<button className='card-btn'>READ MORE</button>}>
+          <div>This app was made in React.js</div>
+        </Popup>
     </div>
   );
 };
